f67d7901d60db545f38662f96ccb4c923708c8e8
Song S.H <37033795+clastro@users.noreply.github.com>
2022-06-09 15:45:36 +0900

    Update Pool.py

--- MultiProcess/Pool.py
+++ MultiProcess/Pool.py
@@ -3,6 +3,9 @@ from tqdm import tqdm
 import time
 import os
 
+def f(id_value):
+    return 1
+
 with Pool(processes=15) as pool:
     results = [pool.apply_async(f, args=(i, )) for i in df_pqrst_afib['unique_id']]
     results = [r.get() for r in tqdm(results)]
